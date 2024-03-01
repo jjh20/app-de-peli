b21f743f8bf2524eb19428e8f3e5dada41ed6001
Jael Jimenez <jimenezherasmej@gmail.com>
2024-03-01 21:48:24 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'App-de-peli',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
